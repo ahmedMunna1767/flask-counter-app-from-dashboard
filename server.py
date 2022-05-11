0abfb3510c67951fcc2618d3f816b795f42ccef5
Ahmed Bin Nasser <98182515+ahmedMunna1767@users.noreply.github.com>
2022-05-12 02:29:53 +0600

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
-db_cache = redis.Redis(host='redis-mogenius-t71j5x', port=6379)
+db_cache = redis.StrictRedis(host="redis-mogenius-t71j5x", port=6379, db=0, password='Salmaahmed1767')
 
 def get_hit_count():
     retries = 5
