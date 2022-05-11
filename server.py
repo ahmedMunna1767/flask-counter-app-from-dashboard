b62623dff29e9b3e1efbd5081f9bac39e411a07a
Ahmed Bin Nasser <98182515+ahmedMunna1767@users.noreply.github.com>
2022-05-12 02:06:54 +0600

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask
 server = Flask(__name__)
 
-@server.route("/")
+count = 0
+# def get_hit_count():
+#     retries = 5
+
+#     while True:
+#         try:
+#             return db_cache.incr(name='hit_count')
+#         except redis.exceptions.ConnectionError as exc:
+#             if retries == 0:
+#                 return exc
+#             retries = retries - 1
+#             time.sleep(.5)
+
+
+@server.route('/')
 def hello():
-    return "Hello World!"
+    count = count + 1 
+#     get_hit_count()
+    return "<h1>Page Hit Count {} times. REFRESH MORE!!!</h1>".format(count)
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)
